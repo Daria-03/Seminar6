21b5acea20a02066294db5fddfcd8516132b596c
Daria-03 <kvzss.rz.07.pa@mail.ru>
2023-02-13 15:25:40 +0600

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+int[] CreateArr(int number)
+{
+    System.Console.Write("Левая граница рандомных чисел: ");
+    int start = Convert.ToInt32(Console.ReadLine());
+    System.Console.Write("Правая граница рандомных чисел: ");
+    int stop = Convert.ToInt32(Console.ReadLine());
+
+    int[] Arr = new int[number];
+    for (int i = 0; i < Arr.Length; i++)
+    {
+        Arr[i] = new Random().Next(start, stop + 1);
+    }
+    return Arr;
+}
+
+
+int[] ReversArr(int[] Array)
+{
+  for (int i = 0, j = Array.Length - 1; i < Array.Length / 2; i++, j--)
+    {
+        int tmp = Array[j];
+        Array[j] = Array[i];
+        Array[i] = tmp;
+    }
+    return Array;
+}
+  
+int[] res = CreateArr(5);
+System.Console.WriteLine(String.Join(" ", res));
+System.Console.WriteLine(String.Join(" ", ReversArr(res)));
---
+++ Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+string b = Convert.ToString(x,2);
+Console.WriteLine(b);
